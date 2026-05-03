6d20ae88b2db562bfcad7accf58e5b455c0f3cb6
Filip <schedrivyylolkek@gmail.com>
2026-05-03 21:05:30 +0100

    fixes

--- app/src/main/java/com/example/notesappwidget/data/AppDatabase.kt
+++ app/src/main/java/com/example/notesappwidget/data/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.notesappwidget.data.Note
 
-@Database(entities = [Note::class], version = 1)
+@Database(entities = [Note::class], version = 1,  exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun noteDao(): NoteDao

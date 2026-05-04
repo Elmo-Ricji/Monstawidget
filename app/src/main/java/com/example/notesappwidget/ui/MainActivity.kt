078ba3bd9173b7d8525bb61eeec46ed30f7df6fc
Filip <schedrivyylolkek@gmail.com>
2026-05-04 19:19:09 +0100

    .

--- app/src/main/java/com/example/notesappwidget/ui/MainActivity.kt
+++ app/src/main/java/com/example/notesappwidget/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.notesappwidget.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
